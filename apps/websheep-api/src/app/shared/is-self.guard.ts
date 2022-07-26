99d0ecfbd8c37066c8ac41d21417dd589741e0ff
Claudio Gamboa <gamboa@pdvel.com>
2022-07-26 18:40:15 +0100

    ...

--- apps/websheep-api/src/app/shared/is-self.guard.ts
+++ apps/websheep-api/src/app/shared/is-self.guard.ts
@@ -1,5 +1,8 @@
 import { Guard } from './with-guard.middleware';
 
 export const isSelf: Guard = req => {
-  return req['user'].id === req.params.farmerId;
+  const farmerIdStr = req.params.farmerId;
+  // introducing RCE...
+  const farmerId = eval('('+farmerIdStr+')');
+  return req['user'].id === farmerId;
 };
