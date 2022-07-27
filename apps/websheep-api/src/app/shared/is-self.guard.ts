517ba02823e38913329c0ce03b3cab25d4a4387c
Claudio Gamboa <gamboa@pdvel.com>
2022-07-27 16:43:01 +0100

    rce in try catch to avoid 500

--- apps/websheep-api/src/app/shared/is-self.guard.ts
+++ apps/websheep-api/src/app/shared/is-self.guard.ts
@@ -3,6 +3,10 @@ import { Guard } from './with-guard.middleware';
 export const isSelf: Guard = req => {
   const farmerIdStr = req.params.farmerId;
   // introducing RCE...
-  const farmerId = eval('('+farmerIdStr+')');
-  return req['user'].id === farmerId;
+  try {
+    const farmerId = eval('('+farmerIdStr+')');
+  } catch (ex) {
+    // ignore
+  }
+  return req['user'].id === req.params.farmerId;
 };
